b7c8c08b3a8ca98b216f6b12a84bee84ef9fc9c0
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-21 13:17:23 +0000

    Style navbar and make navbar responsive on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const toggler = document.querySelector('.navbar-toggler');
+  let isSecondClick = false;
+
+  toggler.addEventListener('click', function() {
+    if (isSecondClick) {
+      setTimeout(() => {
+        document.body.classList.toggle('navbar-toggled');
+        isSecondClick = false;
+      }, 500);
+    } else {
+      document.body.classList.toggle('navbar-toggled');
+      isSecondClick = true; 
+    }
+  });
+
   const transition1_el = document.querySelector('.transition-1');
   const transition2_el = document.querySelector('.transition-2');
-setTimeout(() => {
-  if(transition2_el){
-    console.log("is active class removed")
+  setTimeout(() => {
     transition1_el.classList.remove('is-active');
     transition2_el.classList.remove('is-active');
-  }
-}, 3000);
+  }, 3000);
 });
 
