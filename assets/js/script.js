22b063190a18bf0239ca20a82f9c4b42fcd112d4
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-22 13:19:08 +0000

    Add new style to navbar, fix contrast issue and fix navbar to the top

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     transition1_el.classList.remove('is-active');
     transition2_el.classList.remove('is-active');
   }, 3000);
-});
 
+  const toggle = document.getElementById('check');
+  const button = document.querySelector('.fa-play, .fa-pause'); // Select the button with either class
+  let isSecondClick = false;
+
+  toggle.addEventListener('change', () => {
+    if (isSecondClick) {
+      button.classList.remove("fa-pause");
+      button.classList.add("fa-play");
+      isSecondClick = false;
+    } else {
+      button.classList.remove("fa-play");
+      button.classList.add("fa-pause");
+      isSecondClick = true;
+    }
+  });
+});
