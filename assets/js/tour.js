e9523166f10138b5f84a43bf94c12e6956cbb8db
stephenbeese <stephenbeese1@live.co.uk>
2024-09-22 10:38:34 +0100

    Add lesson page tour with driver.js

---
+++ assets/js/tour.js
@@ -0,0 +1,54 @@
+const driver = window.driver.js.driver;
+
+const driverObj = driver({
+    showProgress: true,
+    steps: [
+        {
+            element: "#piano-keys",
+            popover: {
+                className: "first-step-popover-class",
+                title: "Piano Keys",
+                description: "This is the piano. Click a key to hear its sound and match it with the flashcard.",
+                position: "top"
+            }
+        },
+        {
+            element: "#start-game",
+            popover: {
+                className: "first-step-popover-class",
+                title: "Start Game Button",
+                description: "Click here to start the flashcard game and test your note recognition skills.",
+                position: "top"
+            }
+        },
+        {
+            element: "#instructionButton",
+            popover: {
+                className: "first-step-popover-class",
+                title: "How to Play",
+                description: "Click this button to read the game instructions before you begin.",
+                position: "top"
+            }
+        },
+        {
+            element: ".volume-slider",
+            popover: {
+                className: "first-step-popover-class",
+                title: "Volume Control",
+                description: "Use this slider to adjust the volume of the piano sound.",
+                position: "top"
+            }
+        },
+        {
+            element: ".key-checkbox",
+            popover: {
+                className: "first-step-popover-class",
+                title: "Show Key Names",
+                description: "Toggle this checkbox to show or hide the names of the piano keys.",
+                position: "top"
+            }
+        },
+    ]
+});
+
+driverObj.drive();
